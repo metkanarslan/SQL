75604ee67147be8178276905447b6b26643e39c8
metkanarslan <metkan_arslan01@hotmail.com>
2023-11-12 17:51:40 +0300

    k
    k

---
+++ odev8/update-delete.sql
@@ -0,0 +1,45 @@
+UPDATE employee 
+	SET name = 'Ahmet'
+WHERE name ='Yettie Verny';
+
+UPDATE employee
+	SET birthday = '1993-10-10'
+WHERE email ='eszwarc0@mapy.cz'; 
+
+UPDATE employee
+	SET id = 100
+WHERE id = 50;
+
+UPDATE employee
+	SET email = '12321@123412.net'
+WHERE birthday = '1920-01-27';
+
+UPDATE employee
+	SET name = 'Jaffra'
+WHERE name = 'Di Jaffra';
+
+DELETE FROM employee
+WHERE name ='Janeva';
+
+DELETE FROM employee
+WHERE id = 15;
+
+DELETE FROM employee
+WHERE birthday = '1944-05-16';
+
+DELETE FROM employee
+WHERE email = 'nsavell10@archive.org';
+
+DELETE FROM employee
+WHERE id = 10;
+
+
+
+
+
+
+
+
+
+
+
